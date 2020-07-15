397cd09ad8651d4c38376fb0766db1dba7aba6b0
adamjanas <a.janas@protonmail.com>
2020-07-15 12:29:12 +0200

    formed (offer, application) model setup

--- api/structure/models.py
+++ api/structure/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from api.users.models import User
+from django.utils import timezone
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+class Offer(models.Model):
+    title = models.CharField(max_length=120)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    period = models.CharField(max_length=18, help_text='e.g (1 month, 6 months, 1 year)')
+    salary = models.DecimalField(max_digits=8, decimal_places=2)
+    country = CountryField()
+    offer_date = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.title
+
+
+class Application(models.Model):
+    offer = models.ForeignKey('Offer', on_delete=models.CASCADE, related_name='applications')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField
+    content = models.TextField()
+    application_date = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return f"{self.offer} - {self.title}"
