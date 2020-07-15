ab03a17f56c6fae6c9365f763b18cccb772f5fdd
adamjanas <a.janas@protonmail.com>
2020-07-15 10:46:17 +0200

    deleted useless imports

--- api/users/models.py
+++ api/users/models.py
@@ -3,10 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from phone_field import PhoneField
 from django_countries.fields import CountryField
 from api.core.constants import Sex
-from rest_framework.authtoken.models import Token
-from django.conf import settings
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 
 
 class User(AbstractUser):
