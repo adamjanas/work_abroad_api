e1606ce6f85e0f3dc72ab57d2ceefc4b96eca273
adamjanas <a.janas@protonmail.com>
2020-07-16 17:39:08 +0200

    modified  service

--- api/structure/models.py
+++ api/structure/models.py
@@ -2,26 +2,28 @@ from django.db import models
 from api.users.models import User
 from django.utils import timezone
 from django_countries.fields import CountryField
-
+import datetime
 
 class Offer(models.Model):
     title = models.CharField(max_length=80)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    period = models.CharField(max_length=18, help_text='e.g (1 month, 6 months, 1 year)')
+    start_date = models.DateField(default=datetime.date.today())
+    finish_date = models.DateField(default=datetime.date.today())
     salary = models.DecimalField(max_digits=8, decimal_places=2)
     country = CountryField()
     offer_date = models.DateTimeField(default=timezone.now())
 
     def __str__(self):
-        return self.title
+        return f"{self.title} - {self.author}"
 
 
 class Application(models.Model):
     offer = models.ForeignKey('Offer', on_delete=models.CASCADE, related_name='applications')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    applicant = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=80)
     content = models.TextField()
+    attachment = models.FileField(help_text='attach your cv')
     application_date = models.DateTimeField(default=timezone.now())
 
     def __str__(self):
--- settings/base.py
+++ settings/base.py
@@ -145,3 +145,6 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = 'users.User'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
