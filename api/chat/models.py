64cd409580eeec6c90e488c289b42ee93a52c503
adamjanas <a.janas@protonmail.com>
2020-07-17 11:25:29 +0200

    created chat service

--- api/chat/models.py
+++ api/chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from api.users.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Message(models.Model):
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    attachment = models.FileField(null=True, blank=True, help_text='attach some files')
+    message_date = models.DateTimeField(default=timezone.now())
--- api/chat/views.py
+++ api/chat/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics, permissions
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
+from rest_framework import mixins
+from api.core.mixins import ActionPermissionMixin
+from api.core.permissions import IsAuthor
+from api.chat.models import Message
+from api.chat.serializers import MessageSerializer
 
-# Create your views here.
+
+class MessageViewSet(ActionPermissionMixin, viewsets.ModelViewSet):
+    permission_classes_by_action = {
+        "default": (IsAuthenticated,),
+        "update": (IsAuthor,),
+        "partial_update": (IsAuthor,),
+        "destroy": (IsAuthor,)
+    }
+
+    serializer_class = MessageSerializer
+    queryset = Message.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
+
