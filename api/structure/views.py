4d334113d3135c052d679c515530e07a62b49541
adamjanas <a.janas@protonmail.com>
2020-07-21 12:34:11 +0200

    added filters (messages, offers, applications), setup pagination

--- api/structure/views.py
+++ api/structure/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework import mixins
 from api.core.mixins import ActionPermissionMixin
 from api.core.permissions import IsAuthor
+from django_filters import rest_framework as filters
 from api.structure.models import (
     Offer,
     Application,
@@ -26,6 +27,11 @@ class OfferViewSet(ActionPermissionMixin, viewsets.ModelViewSet):
     }
     serializer_class = OfferSerializer
     queryset = Offer.objects.all()
+    filter_fields = {
+        'author': ['exact'],
+        'title': ['exact', 'contains'],
+        'content': ['contains']
+    }
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -40,6 +46,11 @@ class ApplicationViewSet(ActionPermissionMixin, viewsets.ModelViewSet):
     }
     serializer_class = ApplicationSerializer
     queryset = Application.objects.all()
+    filter_fields = {
+        'applicant': ['exact'],
+        'title': ['exact', 'contains'],
+        'content': ['contains']
+    }
 
     def perform_create(self, serializer):
         serializer.save(applicant=self.request.user)
