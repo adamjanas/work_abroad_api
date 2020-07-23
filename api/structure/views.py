4dc7fbdd2fcb5ff3bc4e9a7b813f65d3f5ddaf99
adamjanas <a.janas@protonmail.com>
2020-07-23 15:55:26 +0200

    testing structure of the api

--- api/structure/views.py
+++ api/structure/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, generics, permissions
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework import mixins
 from api.core.mixins import ActionPermissionMixin
-from api.core.permissions import IsAuthor
+from api.core.permissions import IsAuthor, IsApplicant
 from api.structure.models import (
     Offer,
     Application,
@@ -34,9 +34,9 @@ class OfferViewSet(ActionPermissionMixin, viewsets.ModelViewSet):
 class ApplicationViewSet(ActionPermissionMixin, viewsets.ModelViewSet):
     permission_classes_by_action = {
         "default": (IsAuthenticated,),
-        "update": (IsAuthor,),
-        "partial_update": (IsAuthor,),
-        "destroy": (IsAuthor,),
+        "update": (IsApplicant,),
+        "partial_update": (IsApplicant,),
+        "destroy": (IsApplicant,),
     }
     serializer_class = ApplicationSerializer
     queryset = Application.objects.all()
