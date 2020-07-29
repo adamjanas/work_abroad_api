9c0e53c14e5ab06b7d71cf097587de4ec739c260
adamjanas <a.janas@protonmail.com>
2020-07-29 13:16:12 +0200

    modified pipelines settings

--- settings/pipelines.py
+++ settings/pipelines.py
@@ -6,11 +6,15 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default')
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ.get("DB_NAME", "postgres"),
-        "USER": os.environ.get("DB_USER", "postgres"),
-        "PASSWORD": os.environ.get("DB_PASSWORD", "password"),
-        "HOST": os.environ.get("DB_HOST", "database"),
-        "PORT": os.environ.get("DB_PORT", 5432),
-
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "172.17.0.1",
+        "PORT": "password",
     }
 }
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+
+# let's speed up tests a little
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
